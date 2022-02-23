1d5ef5916448ce40eee2ae17f394d302a0e84b98
karen okasaki <oks.karen@gmail.com>
2022-02-23 09:51:06 -0300

    content router set up

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const app = express();
 
 app.use(express.json())
 
-
+const contentRouter = require("./routes/content.routes")
+app.use('/content', contentRouter)
 
 
 
 app.listen(Number(process.env.PORT), () => { //acessando a variável de objeto no dotenv
     console.log(`Server up and running at port ${process.env.PORT}`)
-})                         
+})
 
 
